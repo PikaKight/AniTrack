76b240d824708fb1485e4f4ec107056e62b3b87d
PikaKight <tmmarcus73@gmail.com>
2020-04-30 22:13:43 -0400

    stuff

--- anime.py
+++ anime.py
@@ -1,16 +1,19 @@
 class Anime:
     anime_list = {}
 
+    def view_all(self):
+        return Anime.anime_list
+
     def view(self, anime: str):
         return Anime.anime_list[anime]
     
     def add_new(self, anime: str):
-        Anime.anime_list[anime] = {}
+        Anime.anime_list[anime] = {"Ep": 0}
     
     def add_episode(self, anime: str, num: int):
-        Anime.anime_list[anime]["Ep"] += nums
+        Anime.anime_list[anime]["Ep"] += num
+
+    def remove(self, anime: str):
+        
         
 
-Anime().add_new("SAO")
-Anime().add_episode("SAO", 20)
-print(Anime().view())
